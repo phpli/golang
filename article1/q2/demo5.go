4f95d1e3db79fbc0b0286eaa4d0e3e67ae3cb8fb
phpi <lichangdong100@gmail.com>
2019-05-17 11:35:39 +0800

    测试自己写的模块引用

---
+++ article1/q2/demo5.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"github.com/astaxie/beego"
+	"article1/q2/lib"
+)
+
+var name  string
+
+func init()  {
+	flag.StringVar(&name,"name","everyone","The greeting object.")
+}
+
+func main()  {
+	flag.Parse()
+	//beego.Run()
+	lib.Hello(name)
+}
