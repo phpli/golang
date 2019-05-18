9f5770ed2a8a3179db1bfb66b6bf196b303abfaa
phpi <lichangdong100@gmail.com>
2019-05-18 22:14:10 +0800

    类型断言

---
+++ article2/q3/demo9.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main()  {
+	var err error
+	n,err := io.WriteString(os.Stdout,"Hello ,everyone!\n")
+	if err !=nil{
+		fmt.Printf("Error:%v!\n",err)
+	}
+	fmt.Printf("%d byte(s) were writer.\n",n)
+}
