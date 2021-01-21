e7ead18689c2b724e07acb9e410ff021d2c166fb
phpli <lichangdong100@gmail.com>
2021-01-21 18:19:03 +0800

    'copy'

---
+++ article21/q1/demo75.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main()  {
+	type Ta []int
+	type Tb []int
+	des := Ta{1,2,3}
+	src := Tb{5,6,7,8,9}
+	n := copy(des, src) // copy 返回的数据 是复制元素数量  copy函数的第一个参数为目标切片，第二个参数为源切片。
+	//此时 des{5,6,7}
+
+	fmt.Println(n,des,cap(des))
+	fmt.Println(n,src)
+
+	fmt.Println(des[1:]) //des[1:] -->6 7
+	fmt.Println(des)//此时 des{5,6,7}
+	n = copy(des[1:], des)
+	fmt.Println(des)//为啥变成[5,5,6]
+	fmt.Println(des[1:],cap(des[1:]))
+
+	a :=[4]int{}
+	n = copy(a[:], src)
+	fmt.Println(n, a)
+	n = copy(a[:], a[2:])
+	fmt.Println(n,a)
+}
