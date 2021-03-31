42faf33828c0f625dcbfa52a92b2497be68c59ea
phpli <lichangdong100@gmail.com>
2021-03-31 10:33:18 +0800

    'interface'

--- article21/q1/demo89.go
+++ article21/q1/demo89.go
@@ -1,3 +1,19 @@
 package main
 
+import "fmt"
 
+func main()  {
+	// 编译器将把123的类型推断为内置类型int。
+	var x interface{} = 123
+	// 情形一：
+	n, ok := x.(int)
+	fmt.Println(n, ok)
+	n = x.(int)
+	fmt.Println(n)
+
+	// 情形二：
+	a, ok := x.(float64)
+	fmt.Println(a, ok)
+	// 情形三：
+	a = x.(float64) // 将产生一个恐慌
+}
